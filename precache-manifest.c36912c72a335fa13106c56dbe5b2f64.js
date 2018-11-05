55bd79c5cda54789a53f8e54476afd30f2837fc1
Alexander Mostovenko <alexmost1989@gmail.com>
2018-11-05 22:43:00 +0200

    update book

---
+++ precache-manifest.c36912c72a335fa13106c56dbe5b2f64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CRA-precompile-example/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5388b2d256cf81d17276",
+    "url": "/CRA-precompile-example/static/js/runtime~main.5388b2d2.js"
+  },
+  {
+    "revision": "4fd0eb3a7cab7121d45e",
+    "url": "/CRA-precompile-example/static/js/main.4fd0eb3a.chunk.js"
+  },
+  {
+    "revision": "400677c08f66c78e0236",
+    "url": "/CRA-precompile-example/static/js/1.400677c0.chunk.js"
+  },
+  {
+    "revision": "4fd0eb3a7cab7121d45e",
+    "url": "/CRA-precompile-example/static/css/main.9b1dff35.chunk.css"
+  },
+  {
+    "revision": "fbe2d6366d302c421c5b51a8858d88f5",
+    "url": "/CRA-precompile-example/index.html"
+  }
+];
---
+++ uk/precache-manifest.55fdd1a6f5f99705f43d76d9ac49966c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CRA-precompile-example/uk/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "17940000e5c29f158e5f",
+    "url": "/CRA-precompile-example/uk/static/js/runtime~main.17940000.js"
+  },
+  {
+    "revision": "d019bebdbef2f5debfd1",
+    "url": "/CRA-precompile-example/uk/static/js/main.d019bebd.chunk.js"
+  },
+  {
+    "revision": "1d216390b8128bbad392",
+    "url": "/CRA-precompile-example/uk/static/js/1.1d216390.chunk.js"
+  },
+  {
+    "revision": "d019bebdbef2f5debfd1",
+    "url": "/CRA-precompile-example/uk/static/css/main.6a1bbe94.chunk.css"
+  },
+  {
+    "revision": "9fadc17e70f56a241d0bb41910057c7c",
+    "url": "/CRA-precompile-example/uk/index.html"
+  }
+];
